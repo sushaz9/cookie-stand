3a349b8292c3099809578d7387b5d9012d960ceb
sushaz9 <susannahelisabeth1@gmail.com>
2023-09-28 15:54:29 +0100

    change js html and css

--- app.js
+++ app.js
@@ -69,6 +69,11 @@ Shop.prototype.render = function () {
     tr.appendChild(td);
   }
 
+  //add total to end of row
+  const totalTd = document.createElement("td");
+  totalTd.textcontent = this.totalCookiesSold;
+  tr.appendChild(totalTd);
+
   // add that row to the table
   table.appendChild(tr);
 };
@@ -80,7 +85,7 @@ const dubai = new Shop("Dubai", 11, 38, 3.7);
 const paris = new Shop("Paris", 20, 38, 2.3);
 const lima = new Shop("Lima", 2, 16, 4.6);
 
-// claculate sales for each store (commented out because the calculate sales in the render method)
+// calculate sales for each store (commented out because the calculate sales in the render method)
 // seattle.calculateSales()
 // tokyo.calculateSales()
 // dubai.calculateSales()
@@ -88,18 +93,23 @@ const lima = new Shop("Lima", 2, 16, 4.6);
 // lima.calculateSales()
 
 // //create top row
-// const headerRow = document.createElement("tr");
-// const blankTd = document.createElement {
-// for(let i = 0; i < hours.length; i++)
-// const th = document.createElement("th");
-// th.textContent = hours[i];
-// headerRow.appendChild(th)
+const headerRow = document.createElement("tr");
+const blankTd = document.createElement("td");
+headerRow.appendChild(blankTd);
 
-// }
+for (let i = 0; i < hours.length; i++) {
+  const th = document.createElement("th");
+  th.textContent = hours[i];
+  headerRow.appendChild(th);
+}
 
-// const totalHeading = document.createElement()
+// add a total heading
+const totalHeading = document.createElement("th");
+totalHeading.textContent = "Total";
+headerRow.appendChild(totalHeading);
 
-// table.appendChild(headerRow)
+// add the row to the table
+table.appendChild(headerRow);
 
 // render each store on the page
 seattle.render();
@@ -107,3 +117,34 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+
+const form = document.querySelector("form");
+
+// we are listening to the submit event of our form
+form.addEventListener("submit", function (event) {
+  // the default behaviour of submit is:
+  // - refresh the page
+  // - add information to the url
+  // so here we are preventing that from happening
+  event.preventDefault();
+
+  // we are collecting the information from the event object to get the users inputs
+  const username = event.target.username.value;
+  const email = event.target.email.value;
+
+  // using DOM manipulation to add the response to the page (just so we can see it)
+  const submit = document.getElementById("submit");
+  const p = document.createElement("p");
+  p.textContent = `${username} with the email ${email} has requested to be contacted.`;
+  submit.appendChild(p);
+});
+
+// get my button DOM node
+// querySelector gets the FIRST item that matches the selector
+const button = document.querySelector("button");
+
+// button has an event listener, that when you click, run the callback function (cbf)
+button.addEventListener("click", function () {
+  button.classList.toggle("salmon");
+  button.classList.toggle("bisque");
+});
